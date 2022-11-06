c93155fd3e383820a3eac7da150f1a2b100d0c62
MrValk <n.s.peters05@gmail.com>
2022-11-06 23:40:37 +0100

    Removed some WorkBox leftovers.

--- workbox-config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	globDirectory: 'build/',
-	globPatterns: ['**/*.{css,js,json,svg,png,html}'],
-	swDest: 'build/service-worker.js',
-	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/]
-};
